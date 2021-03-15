a67cdd2c60e101e28a24d937f7338e270c505ba3
Fergal Moran <fergal.moran@gmail.com>
2021-03-15 20:12:59 +0000

    15Mar

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -11,5 +11,19 @@ def index():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+
+@app.route('/api/servers')
+def get_servers():
+    servers = [
+        {'name': 'server1', 'description': 'My number 1 server',
+            'image': 'http://placebeard.it/64/64?beard=1'},
+        {'name': 'server2', 'description': 'My number 2 server',
+            'image': 'http://placebeard.it/64/64?beard=2'},
+        {'name': 'server3', 'description': 'My number 3 server',
+            'image': 'http://placebeard.it/64/64?beard=3'},
+    ]
+    return jsonify(servers)
+
 
 app.run(debug=True)
